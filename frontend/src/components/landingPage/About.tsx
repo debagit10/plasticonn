bf3f871da99245aed43eaa0ca3460f2f80b5da3e
Josiah Elias <josiahelias11@gmail.com>
2024-09-08 23:33:58 +0100

    feat: about page almost done, picture left

--- frontend/src/components/landingPage/About.tsx
+++ frontend/src/components/landingPage/About.tsx
@@ -1,7 +1,47 @@
+import { Typography } from "@mui/material";
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="bg-[#D9F0DA] grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div>Hi</div>
+      <div className="m-10">
+        <Typography fontWeight={700} color="#047308" variant="h5">
+          About Plasticonn
+        </Typography>
+        <div className="lg:w-[80%] mt-5  ">
+          <Typography variant="body2" fontWeight={400}>
+            Our mission is to facilitate connections and bridge the gap between
+            plastic collectors and plastic collection centers by leveraging
+            geolocation technology and citizens science.By streamlining the
+            plastic recycling process, we aim to enhance plastic waste
+            management and promote a collaborative approach to environment
+            sustainability.
+          </Typography>
+          <Typography
+            fontWeight={700}
+            variant="subtitle2"
+            sx={{ marginTop: "1.25rem" }}
+          >
+            <span className="text-[#047308]">What do people </span>have to say
+            about us?
+          </Typography>
+          <div className="border-[#047308] border p-5 -rotate-2 rounded-lg">
+            <Typography variant="body2">
+              <ul>
+                <li> - I will like to use your data</li>
+                <li> - How can I be part of the system?</li>
+                <li>
+                  {" "}
+                  - Can i get connected to plastic recycling in ogun state?
+                </li>
+              </ul>
+            </Typography>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
