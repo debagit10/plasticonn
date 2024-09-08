2cc264c5d91a44b6a1ca7ca87f0c70c9d2c0d3dc
Josiah Elias <josiahelias11@gmail.com>
2024-09-08 22:50:53 +0100

    fix: something fr

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,4 +1,4 @@
-import { Stack, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import React from "react";
 import CollectionCenters from "../components/landingPage/dashboard/CollectionCenters";
 import Collectors from "../components/landingPage/dashboard/Collectors";
