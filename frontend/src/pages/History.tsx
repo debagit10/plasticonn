0c7492c206e433bba9580f2fda2c0fa3ede9a483
Josiah Elias <josiahelias11@gmail.com>
2024-09-11 19:57:03 +0100

    feat: history design and structure implemented

--- frontend/src/pages/History.tsx
+++ frontend/src/pages/History.tsx
@@ -1,3 +1,15 @@
+import {
+  TableContainer,
+  Paper,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Chip,
+  Typography,
+} from "@mui/material";
+import { radiansToLength } from "@turf/turf";
 import React, { useEffect } from "react";
 import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
@@ -6,12 +18,76 @@ import Side_nav_container from "../containers/Side_nav_container";
 const History = () => {
   const [cookies, setCookies] = useCookies();
   const navigate = useNavigate();
+
+  const historyList = [
+    { centerID: "1A2B3C", time: "5:39AM", status: false },
+    { centerID: "1A2B3C", time: "5:39AM", status: true },
+    { centerID: "1A2B3C", time: "5:39AM", status: false },
+    { centerID: "1A2B3C", time: "5:39AM", status: true },
+    { centerID: "1A2B3C", time: "5:39AM", status: false },
+    { centerID: "1A2B3C", time: "5:39AM", status: true },
+    { centerID: "1A2B3C", time: "5:39AM", status: false },
+    { centerID: "1A2B3C", time: "5:39AM", status: true },
+    { centerID: "1A2B3C", time: "5:39AM", status: false },
+    { centerID: "1A2B3C", time: "5:39AM", status: true },
+  ];
+
   useEffect(() => {
     if (!cookies.token) {
       navigate(`/login-${cookies.role}`);
     }
   });
-  return <Side_nav_container>History</Side_nav_container>;
+  return (
+    <Side_nav_container>
+      <div className="mx-10 my-5">
+        <Typography
+          variant="h6"
+          sx={{
+            textDecoration: "underline",
+            color: "#0B490D",
+            textDecorationColor: "#0B490D",
+          }}
+        >
+          History
+        </Typography>
+
+        <TableContainer component={Paper} sx={{ marginTop: "1rem" }}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  {cookies.role === "collector" ? "Center ID" : "Collector ID"}
+                </TableCell>
+                <TableCell>Time</TableCell>
+                <TableCell>Status</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {historyList.map((row, index) => (
+                <TableRow
+                  key={index}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  className="cursor-pointer"
+                >
+                  <TableCell>{row.centerID}</TableCell>
+                  <TableCell>{row.time}</TableCell>
+                  <TableCell>
+                    <Chip
+                      label={row.status ? "accepted" : "rejected"}
+                      sx={{
+                        backgroundColor: row.status ? "#a5d6a7" : "#ef9a9a",
+                        color: row.status ? "#2e7d32" : "#c62828",
+                      }}
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </Side_nav_container>
+  );
 };
 
 export default History;
