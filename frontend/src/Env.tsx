eef6aa36f434c37b4cf54c267bbf5f3f548974c1
Josiah Elias <josiahelias11@gmail.com>
2025-05-09 20:37:36 +0100

    fix: temporary changes

--- frontend/src/Env.tsx
+++ frontend/src/Env.tsx
@@ -1,5 +1,5 @@
 const BASE_PROD_API_URL = "https://plasticonn-backend.onrender.com";
-const CLIENT_ENV = "dev";
+const CLIENT_ENV = "prod";
 const BASE_DEV_API_URL = "http://localhost:5500";
 
 export default { BASE_PROD_API_URL, BASE_DEV_API_URL, CLIENT_ENV };
